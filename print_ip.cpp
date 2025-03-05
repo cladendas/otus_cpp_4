78d35acff0307522e7ffb05a99ef87fc0f2e3f35
cladendas <rvdagi05@yandex.ru>
2025-03-05 20:16:37 +0300

    Update print_ip.cpp

--- print_ip.cpp
+++ print_ip.cpp
@@ -1,12 +1,18 @@
+/**
+    \brief Печать ip-адреса
 
-// print_ip( int8_t{-1} ); // 255 
-// print_ip( int16_t{0} ); // 0.0 
-// print_ip( int32_t{2130706433} ); // 127.0.0.1 
-// print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41 
-// print_ip( std::string{“Hello, World!”} ); // Hello, World! 
-// print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
-// print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
-// print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+    Функция print_ip, которая способна выводить в консоль ip-адрес, который передан ей в типах, указанных далее    
+
+    print_ip( int8_t{-1} ); // 255 
+    print_ip( int16_t{0} ); // 0.0 
+    print_ip( int32_t{2130706433} ); // 127.0.0.1 
+    print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41 
+    print_ip( std::string{“Hello, World!”} ); // Hello, World! 
+    print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
+    print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
+    print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+*/
+
 
 #include <iostream>
 #include <cstdint>
@@ -14,20 +20,41 @@
 #include <list>
 #include <vector>
 
+template <typename Container>
+struct is_container : std::false_type{};
+
+template <typename... Ts> 
+struct is_container<std::list<Ts...>> : std::true_type{};
+
+template <typename... Ts> 
+struct is_container<std::vector<Ts...>> : std::true_type{};
+
+/**
+    \brief Шаблон для вывода ip-адреса из std::vector и std::list
+*/
+template<typename T>
+typename std::enable_if<is_container<T>::value>::type
+print_ip(T value) {
+    for (auto iter = value.begin(); iter != value.end(); iter++) {
+        if (iter != --value.end()) {
+            std::cout << *iter << '.';
+        } else {
+            std::cout << *iter;
+        }
+    }
+    std::cout << std::endl;
+};
+
 template <typename T>
 typename std::enable_if<std::is_arithmetic<T>::value>::type
 print_ip(T value) {
     uint8_t* bytePtr = reinterpret_cast<uint8_t*>(&value);
+    std::vector<int> vec(sizeof(value));
 
     for (size_t i = 0; i < sizeof(value); ++i) {
-
-        if (i == sizeof(value) - 1) {
-            std::cout << static_cast<int>(bytePtr[i]);
-        } else {
-            std::cout << static_cast<int>(bytePtr[i]) << '.';
-        }
+        vec[sizeof(value) - 1 - i] = static_cast<int>(bytePtr[i]);
     }
-    std::cout << std::endl;
+    print_ip(vec);
 }
 
 //-------------------------------------------------//
@@ -37,11 +64,15 @@ struct is_string {
     static const bool value = false;
 };
 
+
 template<>
 struct is_string<std::string> {
     static const bool value = true;
 };
 
+/**
+    \brief Шаблон для вывода ip-адреса из std::string
+*/
 template<typename T>
 typename std::enable_if<is_string<T>::value>::type
 print_ip(T value) {
@@ -58,44 +89,6 @@ struct is_vector_int {
 template<>
 struct is_vector_int<std::vector<int>> {
     static const bool value = true;
-};
-
-template<typename T>
-typename std::enable_if<is_vector_int<T>::value>::type
-print_ip(T value) {
-    for (auto oktet: value) {
-        if (*(value.end() - 1) != oktet) {
-            std::cout << oktet << '.';
-        } else {
-            std::cout << oktet; 
-        }
-    }
-    std::cout << std::endl;
-};
-
-//-------------------------------------------------//
-
-template<typename T>
-struct is_list_short {
-    static const bool value = false;
-};
-
-template<>
-struct is_list_short<std::list<short>> {
-    static const bool value = true;
-};
-
-template<typename T>
-typename std::enable_if<is_list_short<T>::value>::type
-print_ip(T value) {
-    for (auto iter = value.begin(); iter != value.end(); iter++) {
-        if (*iter != value.back()) { 
-            std::cout << *iter << '.';
-        } else {
-            std::cout << *iter;
-        }
-    }
-    std::cout << std::endl;
 };
 
 int main() {
